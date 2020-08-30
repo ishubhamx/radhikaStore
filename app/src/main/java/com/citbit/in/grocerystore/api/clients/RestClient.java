0c8e05dd5f060698c6d113738280a04754a458c1
Shubham <sv615218@gmail.com>
2020-08-30 19:33:31 +0530

    api end point updated

--- app/src/main/java/com/citbit/in/grocerystore/api/clients/RestClient.java
+++ app/src/main/java/com/citbit/in/grocerystore/api/clients/RestClient.java
@@ -25,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RestClient {
 
-    public static final String BASE_URL = "https://citbitstoreapi.herokuapp.com-";//http://<ipaddress>/directoryname
+    public static final String BASE_URL = "https://citbitstoreapi.herokuapp.com";//http://<ipaddress>/directoryname
     public static Retrofit RETROFIT = null;
     public static Retrofit RETROFIT1 = null;
     public static RestService restService = null;
